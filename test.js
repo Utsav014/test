2216ff08499d68003e40247621f56e24db73f7dd
nishil maharjan <nishil.maja@gmail.com>
2023-07-13 20:58:26 +0545

    done

--- test.js
+++ test.js
@@ -1,14 +1,7 @@
 console.log("Pasro Gay");
 console.log("paro");
-<<<<<<< HEAD
-console.log("jsdhfvbhj");
-console.log("jsdhfvbhj");
-console.log("jsdhfvbhj");
-console.log("jsdhfvbhj");
-=======
 console.log("paro");
 console.log("paro");
 console.log("paro");
 console.log("paro");
 console.log("paro");
->>>>>>> ad8befbc265ea4178001f6f727461fe0e0557d79
