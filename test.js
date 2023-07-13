0f15ba61b906efc8a98de9a8bf5a357adee7e021
Utsav Maharjan <mrzn.utsav14@gmail.com>
2023-07-13 20:34:17 +0545

    added

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 console.log("Pasro Gay");
 console.log("paro");
-<<<<<<< HEAD
 console.log("jsdhfvbhj");
 console.log("jsdhfvbhj");
 console.log("jsdhfvbhj");
 console.log("jsdhfvbhj");
-=======
 console.log("paro");
 console.log("paro");
 console.log("paro");
 console.log("paro");
 console.log("paro");
->>>>>>> ad8befbc265ea4178001f6f727461fe0e0557d79
+console.loog("lklklklkl")
+console.loog("lklklklkl")
+console.loog("lklklklkl")
+console.loog("lklklklkl")
+console.loog("lklklklkl")
+console.loog("lklklklkl")
